69ac5a17e87c8ee3efb55566d62fa3412afb6de7
Tomasz Duda <tomek@ddart.pl>
2014-11-29 20:33:37 +0100

    Resolve #16 - Iterator

---
+++ Behavioral/Iterator/output.md
@@ -0,0 +1,8 @@
+```
+string 'Item 1' (length=6)
+string 'Item 2' (length=6)
+string 'Item 3' (length=6)
+string 'Item 1' (length=6)
+string 'Item 2' (length=6)
+string 'Item 3' (length=6)
+```
---
+++ Behavioral/IteratorAggregate/ConcreteAggregate.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * Copyright (c) 2015 DD Art Tomasz Duda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace PowerEcommerce\Pattern\Behavioral\IteratorAggregate {
+    use Traversable;
+
+    /**
+     * Class Aggregate
+     * @package PowerEcommerce\Pattern\Behavioral\IteratorAggregate
+     */
+    class ConcreteAggregate extends Aggregate
+    {
+        /**
+         * @var array
+         */
+        private $_items = [];
+
+        /**
+         * (PHP 5 &gt;= 5.0.0)<br/>
+         * Whether a offset exists
+         * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+         * @param mixed $offset <p>
+         * An offset to check for.
+         * </p>
+         * @return boolean true on success or false on failure.
+         * </p>
+         * <p>
+         * The return value will be casted to boolean if non-boolean was returned.
+         */
+        public function offsetExists($offset)
+        {
+            return array_key_exists($offset, $this->_items);
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.0.0)<br/>
+         * Offset to retrieve
+         * @link http://php.net/manual/en/arrayaccess.offsetget.php
+         * @param mixed $offset <p>
+         * The offset to retrieve.
+         * </p>
+         * @return mixed Can return all value types.
+         */
+        public function offsetGet($offset)
+        {
+            return $this->offsetExists($offset) ? $this->_items[$offset] : null;
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.0.0)<br/>
+         * Offset to set
+         * @link http://php.net/manual/en/arrayaccess.offsetset.php
+         * @param mixed $offset <p>
+         * The offset to assign the value to.
+         * </p>
+         * @param mixed $value <p>
+         * The value to set.
+         * </p>
+         * @return void
+         */
+        public function offsetSet($offset, $value)
+        {
+            $this->_items[$offset] = $value;
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.0.0)<br/>
+         * Offset to unset
+         * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+         * @param mixed $offset <p>
+         * The offset to unset.
+         * </p>
+         * @return void
+         */
+        public function offsetUnset($offset)
+        {
+            unset($this->_items[$offset]);
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.1.0)<br/>
+         * Count elements of an object
+         * @link http://php.net/manual/en/countable.count.php
+         * @return int The custom count as an integer.
+         * </p>
+         * <p>
+         * The return value is cast to an integer.
+         */
+        public function count()
+        {
+            return sizeof($this->_items);
+        }
+
+        /**
+         * (PHP 5 &gt;= 5.0.0)<br/>
+         * Retrieve an external iterator
+         * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+         * @return Traversable An instance of an object implementing <b>Iterator</b> or
+         * <b>Traversable</b>
+         */
+        public function getIterator()
+        {
+            return new \ArrayIterator($this->_items);
+        }
+    }
+}
